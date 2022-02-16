8f287f94ad897fc932b45e9a159f538c606a14a3
jfrenza <>
2022-02-15 21:18:26 -0500

    Unittest
    Unittest

--- OOP.py
+++
@@ -1,5 +0,0 @@
-fosadfnasifnanfa
-
-dfoasfoasifaoisf
-iafoasdoia
-print('hello word')
---
+++ Unit_Test.py
@@ -0,0 +1,16 @@
+import unittest
+
+#Function to test
+
+def times_ten(number):
+    return number * 100
+
+#Test class class
+
+class TestTimesTen(unittest.TestCase):
+    def test_multiply_ten_by_zero(self):
+        self.assertEqual(times_ten(0), 0, 'Expected times_ten(0) to return 0')
+
+#Run the Test
+
+unittest.main()
