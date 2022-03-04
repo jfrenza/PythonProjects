b8341ba7da3395c3270b84227b560ddb6cc9d48f
jfrenza <jfrenzac@gmail.com>
2022-03-04 15:16:01 -0500

    unitest

--- Unit_Test.py
+++ Unit_Test.py
@@ -11,7 +11,12 @@ class TestTimesTen(unittest.TestCase):
     def test_multiply_ten_by_zero(self):
         self.assertEqual(times_ten(0), 0, 'Expected times_ten(0) to return 0')
 
+    def test_multiply_ten_by_ten(self):
+        self.assertEqual(times_ten(10), 100, 'Expected times_ten(10) to return 100')
+
+    def test_multiply_ten_by_minusten(self):
+        self.assertEqual(times_ten(-10), -100, 'Expected times_ten(-10) to return -100')
+
 #Run the Test
 
 unittest.main()
-print('hello word')
