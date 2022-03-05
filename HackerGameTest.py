ffe70bd69ab3d882d2667bd8e1bafdd7a3c9bef9
jfrenza <jfrenzac@gmail.com>
2022-03-05 07:26:52 -0500

    new

--- HackerGameTest.py
+++ HackerGameTest.py
@@ -5,9 +5,11 @@ from HackerGame import fizzBuzz
 
 class TestfizzBuzz(unittest.TestCase):
     def test_fizz_buzz_3(self):
-        self.assertEqual(fizzBuzz(3), [1, 2, 'Fizz'],"Expected HackerGame(3) to return [1, 2, 'Fizz']")
+        self.assertEqual(fizzBuzz(3), [1, 2, 'Fizz'],
+                            "Expected HackerGame(3) to return [1, 2, 'Fizz']")
 
     def test_fizz_buzz_5(self):
-        self.assertEqual(fizzBuzz(5), [1, 2, 'Fizz', 4, 'Buzz'],"Expected HackerGame(3) to return [1, 2, 'Fizz', 4, 'Buzz']")
+        self.assertEqual(fizzBuzz(5), [1, 2, 'Fizz', 4, 'Buzz'],
+                "Expected HackerGame(3) to return [1, 2, 'Fizz', 4, 'Buzz']")
 
 unittest.main()
